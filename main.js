1d14209065e0be8819bfc082f47f540e1d1d1ebc
PaoloPascucci <pascuccip9@gmail.com>
2021-10-18 14:58:37 +0200

    Add snack 2

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ else if (Num2 > Num1) {
 else {
     document.getElementById("snack_1").innerHTML = "Pareggio";
 }
+
+//snack 2
+
+const word1 = prompt('Inserisci una parola');
+const word2 = prompt('Inseriscine un altra');
+
+if (word1.length > word2.length) {
+    document.getElementById("1").innerHTML = word2;
+    document.getElementById("2").innerHTML = word1;
+}
+else if ((word2.length > word1.length)) {
+    document.getElementById("1").innerHTML = word1;
+    document.getElementById("2").innerHTML = word2;
+}
